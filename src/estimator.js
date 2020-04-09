bfc53f154ff29d0b29d9bf8dd916cf00e9a2164e
Idowu Daramola <idowu.daramola@gmail.com>
2020-04-09 23:27:53 +0100

    computed 'currentlyInfected' and 'infectionsByRequestedTime' for both 'impact' and 'severeImpact' objects

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,44 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+   const input = data;
+
+  
+
+   const impact = {currentlyInfected: input.reportedCases * 10};
+   const severeImpact = {currentlyInfected: input.reportedCases * 50}
+
+  
+
+   const periodType = input.periodType;
+   const timeToElapse = input.timeToElapse;
+
+   if (periodType == "months"){
+       const days =  timeToElapse * 30;
+       const factor = parseInt(days/3, 10);
+       impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
+      severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+   } else if (periodType == "weeks"){
+    const days =  timeToElapse * 7;
+    const factor = parseInt(days/3, 10);
+    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
+   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+
+
+   }else{
+    const days =  timeToElapse;
+    const factor = parseInt(days/3, 10);
+    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
+   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+}
+
+
+    
+   
+   
+    return {
+       data : input,
+       impact, 
+       severeImpact 
+    }
+}
 
 export default covid19ImpactEstimator;
