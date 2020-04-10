b659acc428dffd495e068102348462136462b527
Idowu Daramola <idowu.daramola@gmail.com>
2020-04-10 01:21:47 +0100

    fixed eslint whitespace problem by inserting as additional rule inside .eslintrc.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,44 +1,37 @@
 const covid19ImpactEstimator = (data) => {
-   const input = data;
+  const input = data;
 
-  
 
-   const impact =  {currentlyInfected: input.reportedCases * 10};
-   const severeImpact = {currentlyInfected: input.reportedCases * 50}
+  const impact = { currentlyInfected: input.reportedCases * 10 };
+  const severeImpact = { currentlyInfected: input.reportedCases * 50 };
 
-  
 
-   const periodType = input.periodType;
-   const timeToElapse = input.timeToElapse;
+  const { periodType } = input;
+  const { timeToElapse } = input;
 
-   if (periodType == "months"){
-       const days =  timeToElapse * 30;
-       const factor = parseInt(days/3, 10);
-       impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
-      severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
-   } else if (periodType == "weeks"){
-    const days =  timeToElapse * 7;
-    const factor = parseInt(days/3, 10);
+  if (periodType === 'months') {
+    const days = timeToElapse * 30;
+    const factor = parseInt(days / 3, 10);
     impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
-   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
-
-
-   }else{
-    const days =  timeToElapse;
-    const factor = parseInt(days/3, 10);
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+  } else if (periodType === 'weeks') {
+    const days = timeToElapse * 7;
+    const factor = parseInt(days / 3, 10);
     impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
-   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
-}
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+  } else {
+    const days = timeToElapse;
+    const factor = parseInt(days / 3, 10);
+    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+  }
 
 
-    
-   
-   
-    return {
-       data : input,
-       impact, 
-       severeImpact 
-    }
-}
+  return {
+    data: input,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
